96800a121d8d0ac7ec2a922136fce73c663974a9
Sindre Henriksen <sindrehenriksen93@gmail.com>
2019-03-08 15:49:19 +0100

    Restructure code

--- code/problem6.R
+++ code/problem6.R
@@ -29,14 +29,16 @@ region = 1:length(Y)
 hyper = list(prec=list(prior="loggamma", param=c(alpha, beta)))
 formula = Y ~
   -1 +
-  f(region_struct, model="besag", graph.file=g, constr=FALSE, hyper=hyper) +
+  f(region_struct, model="besag", graph.file=g, constr=FALSE,
+    hyper=hyper) +
   f(region_random, model="iid", hyper=hyper)
 data = list(Y=Y, E=E, region_struct=region, region_random=region)
 result_a = inla(formula, family="poisson", data=data, E=E,
                 control.compute=list(dic=TRUE))
 summary(result_a)
 
-# Plot median of estimate divided by E (Y/E = E*exp(eta)/E = exp(eta)) in map
+# Plot median of estimate divided by E in map
+# (Y/E = E*exp(eta)/E = exp(eta))
 y_median_a = exp(result_a$summary.random$region_struct$`0.5quant`)
 pdf("../figures/y_median_a.pdf")
 germany.plot(y_median_a, col=col, legend=TRUE)
@@ -47,19 +49,27 @@ result_a = inla.hyperpar(result_a)
 
 # Get and smooth marginals
 rns = c(157, 222, 429) #######################################
-kappa_u_marginal = result_a$marginals.hyperpar$`Precision for region_struct`
+kappa_u_marginal = result_a$marginals.hyperpar$
+  `Precision for region_struct`
 kappa_u_marginal = inla.smarginal(kappa_u_marginal)
-kappa_v_marginal = result_a$marginals.hyperpar$`Precision for region_random`
+kappa_v_marginal = result_a$marginals.hyperpar$
+  `Precision for region_random`
 kappa_v_marginal = inla.smarginal(kappa_v_marginal)
 i1 = paste0("index.", rns[1])
 i2 = paste0("index.", rns[2])
 i3 = paste0("index.", rns[3])
-u1_marginal = inla.smarginal(result_a$marginals.random$region_struct[[i1]])
-u2_marginal = inla.smarginal(result_a$marginals.random$region_struct[[i2]])
-u3_marginal = inla.smarginal(result_a$marginals.random$region_struct[[i3]])
-v1_marginal = inla.smarginal(result_a$marginals.random$region_random[[i1]])
-v2_marginal = inla.smarginal(result_a$marginals.random$region_random[[i2]])
-v3_marginal = inla.smarginal(result_a$marginals.random$region_random[[i1]])
+u1_marginal = inla.smarginal(
+  result_a$marginals.random$region_struct[[i1]])
+u2_marginal = inla.smarginal(
+  result_a$marginals.random$region_struct[[i2]])
+u3_marginal = inla.smarginal(
+  result_a$marginals.random$region_struct[[i3]])
+v1_marginal = inla.smarginal(
+  result_a$marginals.random$region_random[[i1]])
+v2_marginal = inla.smarginal(
+  result_a$marginals.random$region_random[[i2]])
+v3_marginal = inla.smarginal(
+  result_a$marginals.random$region_random[[i1]])
 
 # Plot marginals
 p_kappa_u = ggplot(as_tibble(kappa_u_marginal)) +
@@ -99,8 +109,8 @@ p_v3 = ggplot(as_tibble(v3_marginal)) +
 p_marginals = grid.arrange(
   p_kappa_u, p_kappa_v, p_u1, p_v1, p_u2, p_v2, p_u3, p_v3,
   ncol=2)
-ggsave("../figures/posterior_marginals.pdf",
-       plot = p_marginals, width = 5, height = 6, units = "in", dpi = 300)
+ggsave("../figures/posterior_marginals.pdf", plot=p_marginals,
+       width=6, height=7, units="in", dpi=300)
 
 ## ---- 6b
 smoking = read.table("./data/ex2_additionalFiles/smoking.dat")
@@ -109,21 +119,23 @@ data["smoking"] = smoking
 # Define formula and run INLA with smoking as a linear effect
 formula_b_lin = Y ~
   -1 +
-  f(region_struct, model="besag", graph.file=g, constr=FALSE, hyper=hyper) +
+  f(region_struct, model="besag", graph.file=g, constr=FALSE,
+    hyper=hyper) +
   f(region_random, model="iid", hyper=hyper) +
   smoking
-result_b_lin = inla(formula_b_lin, family="poisson", data=data, E=E,
-                    control.compute=list(dic=TRUE))
+result_b_lin = inla(formula_b_lin, family="poisson", data=data,
+                    E=E, control.compute=list(dic=TRUE))
 # summary(result_b_lin)
 
-# Define formula and run INLA with smoking as non-linear effect (rw2)
+# Define formula and run INLA with smoking as a rw2 effect
 formula_b_rw2 = Y ~
   -1 +
-  f(region_struct, model="besag", graph.file=g, constr=FALSE, hyper=hyper) +
+  f(region_struct, model="besag", graph.file=g, constr=FALSE,
+    hyper=hyper) +
   f(region_random, model="iid", hyper=hyper) +
   f(smoking, model="rw2")
-result_b_rw2 = inla(formula_b_rw2, family="poisson", data=data, E=E,
-                    control.compute=list(dic=TRUE))
+result_b_rw2 = inla(formula_b_rw2, family="poisson", data=data,
+                    E=E, control.compute=list(dic=TRUE))
 summary(result_b_rw2)
 
 # Get DIC for all the models
@@ -131,7 +143,7 @@ smoking_none_DIC = result_a$dic$dic
 smoking_lin_DIC = result_b_lin$dic$dic
 smoking_rw2_DIC = result_b_rw2$dic$dic
 
-# Plot posterior median and 95% CI of non-linear effect, compare with linear
+# Plot posterior median and 95% CI of non-linear effect
 smoking_beta = result_b_lin$summary.fixed$mean
 smoking_val = result_b_rw2$summary.random$smoking$ID
 smoking_eff_025 = result_b_rw2$summary.random$smoking$`0.025quant`
@@ -153,8 +165,5 @@ plot_smoking = ggplot(data) +
   geom_line(aes(x, y=ci_high, col="95CI")) +
   # scale_color_manual(values=c("black", "pink", "yellow")) +
   labs(x="smoking", y="effect", col="")
-# plot_smoking
-
-## ---- save
-ggsave("../figures/smoking_effect.pdf",
-       plot = plot_smoking, width = 5.5, height = 3, units = "in", dpi = 300)
+ggsave("../figures/smoking_effect.pdf", plot=plot_smoking,
+       width=5.5, height=3, units="in", dpi=300)
