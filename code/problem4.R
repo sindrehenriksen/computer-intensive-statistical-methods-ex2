dd26e9acd03f972ffba11724b5fee3b57a1bb9a1
nagasjel <moutzebe@gmail.com>
2019-03-14 10:08:56 +0100

    Finish code problem 4

--- code/problem4.R
+++ code/problem4.R
@@ -9,12 +9,28 @@ library(coda)
 library(kableExtra)
 load("data/input.Rdata")
 load("data/mcmc_df.Rdata")
-ess <- effectiveSize(data.frame(MCMC_list$kappa_u[MCMC_list$is_burnin==F],
-                                MCMC_list$kappa_v[MCMC_list$is_burnin==F]))
+ess <- effectiveSize(MCMC_list[MCMC_list$is_burnin==F, 2:9])
 ess
 ## ---- break
 save(file = "../code/data/ess4.Rdata", ess)
 ## ---- essPrint
 load("../code/data/ess4.Rdata")
-cat("ESS[kappa_u] =", ess[1])
-cat("ESS[kappa_v] =", ess[2])
+cat("ESS[v1] =", ess[1])
+cat("ESS[v2] =", ess[2])
+cat("ESS[v3] =", ess[3])
+cat("ESS[u1] =", ess[4])
+cat("ESS[u2] =", ess[5])
+cat("ESS[u3] =", ess[6])
+cat("ESS[kappa_u] =", ess[7])
+cat("ESS[kappa_v] =", ess[8])
+
+
+## ---- relESS
+load("../code/data/samples.Rdata")
+time <- as.numeric(samples$run_time[1])
+relESS <- mean(c(ess[7],ess[8]))/time
+## ---- break
+save(file = "../code/data/ress.Rdata",relESS)
+## ---- rESSprint
+load("../code/data/ress.Rdata")
+cat("Relative effective sample size:", relESS)
